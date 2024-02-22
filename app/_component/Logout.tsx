aadc1e0d4bb6c1c5780b5286e83b8e400a2ebdd7
kwenpicasso <milezkwen@gmail.com>
2024-02-22 20:48:30 +0100

    logout edit

--- app/_component/Logout.tsx
+++ app/_component/Logout.tsx
@@ -12,7 +12,7 @@ const Logout = () => {
   return (
     <div>
        
-        {user ? (<button className='w-[200px]  rounded-sm h-[40px] hover:animate-pulse hover:bg-orange-500 text-white text-sm flex gap-3 justify-center items-center' onClick={() => signOut(() => router.push("/"))}>Sign out <Power/></button>) : ''}
+        {user ? (<button className='w-[200px]  rounded-sm h-[40px] hover:animate-pulse hover:bg-orange-500 border-none outline-none text-white text-sm flex gap-3 justify-center items-center' onClick={() => signOut(() => router.push("/"))}>Sign out <Power/></button>) : ''}
       
     </div>
   )
