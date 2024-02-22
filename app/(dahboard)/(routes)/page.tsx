e44a386961f6995367168c1036d7353b93944fc8
kwenpicasso <milezkwen@gmail.com>
2024-02-22 20:48:16 +0100

    edit

--- app/(dahboard)/(routes)/page.tsx
+++ app/(dahboard)/(routes)/page.tsx
@@ -1,12 +1,11 @@
+import FilterCat from "@/app/_component/FilterCat";
 import Image from "next/image";
 
 
 export default function Home() {
   return (
-<div className="  w-full h-screen   ">
- <div className="">
-hhjhh
- </div>
+<div className="  w-full h-screen pt-3 lg:px-4">
+<FilterCat/>
 </div>
   );
 }
