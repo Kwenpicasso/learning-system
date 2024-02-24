9d96c84cf05238f7b99f7c768f706fded5b9f9c4
kwenpicasso <milezkwen@gmail.com>
2024-02-24 21:54:18 +0100

    enroll section

---
+++ app/_component/EnrollSection.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import React from 'react'
+
+const EnrollSection = () => {
+  return (
+    <div className='w-full h-[200px] rounded-lg shadow-lg flex items-center p-5 bg-white'>
+    <Image src='/hand.png' width={100} height={200} alt='hand'/>
+  <div className='flex flex-col items-center text-center justify-center gap-2'>
+  <h1 className='capitalize font-semibold text-lg'>enroll to this course</h1>
+    <p className='text-xs'>Choose from over 210,000 online video courses with new additions published every month</p>
+    <Button className='bg-green-900 text-orange-500 hover:bg-orange-500 hover:text-green-900'>Enroll Now</Button>
+
+  </div>
+   </div>
+  )
+}
+
+export default EnrollSection
