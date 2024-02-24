8a5d1753b5135a741d527bb545d5dadc3930dc54
kwenpicasso <milezkwen@gmail.com>
2024-02-24 21:53:58 +0100

    video desc comp

---
+++ app/_component/VideoDescription.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { BadgeCheck } from 'lucide-react'
+const VideoDescription = () => {
+  return (
+    <>
+        <h1 className='capitalize font-bold text-black text-lg  mt-1'>next js complete course - full course 2024</h1>
+   <div className='flex gap-2 items-center text-sm mt-1'>
+   <Avatar>
+      <AvatarImage src="/ww.webp" alt="@shadcn"  className='object-cover'/>
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+  <div className='flex flex-col'>
+    <h1 className='font-semibold'>Simoon Grimm</h1>
+    <h1 className='flex items-center gap-1 '>3 Chapters<BadgeCheck size={15} className='text-green-900' /> </h1>
+  </div>
+   </div>
+    </>
+  )
+}
+
+export default VideoDescription
