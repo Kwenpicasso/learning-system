d96a758c557f92f5b7bf276b9fa9e4c87fc70391
kwenpicasso <milezkwen@gmail.com>
2024-02-27 22:37:45 +0100

    courses ts prop

--- typings.d.ts
+++ typings.d.ts
@@ -3,3 +3,17 @@ interface MenuProps {
     icon: string;
     link: string;
 }
+
+interface AllCourseProp{
+    id: string;
+    name: string;
+    author: string;
+    banner: {
+     url: string;
+    };
+    authorimage: {
+        url: string;
+    }
+    totalChapters: number;
+
+}
