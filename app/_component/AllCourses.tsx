42505476ac7718b9a67d811a74070320726c364c
kwenpicasso <milezkwen@gmail.com>
2024-02-27 22:38:39 +0100

    all coures

--- app/_component/AllCourses.tsx
+++ app/_component/AllCourses.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import {
     Avatar,
@@ -5,23 +6,29 @@ import {
     AvatarImage,
   } from "@/components/ui/avatar"
 import { BadgeCheck } from 'lucide-react'
-const AllCourses = () => {
+import Image from 'next/image'
+
+const AllCourses = ({courselists}:{courselists: AllCourseProp[]}) => {
+ 
+  
   return (
-    <div className='w-full mt-3 grid grid-cols-3 gap-2'>
-      <div className='w-[350px] h-[250px] lg:col-span-3 lg:w-full'>
-      <img loading='lazy' src='/nj.webp' alt='courses' className='rounded-lg  object-cover'/>
-      <div className='w-full flex gap-2 justify-start mt-2 items-center'>
-      <Avatar>
-      <AvatarImage src="/ww.webp" alt="@shadcn"  className='object-cover'/>
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
-   <div className='flex flex-col justify-center text-sm capitalize '>
-   <h1 className='font-semibold'>next js complete course - full course 2024</h1>
-   <h1 className='flex gap-1 justify-start items-center'>Simmon Grimm | 3 Chapters <BadgeCheck size={15} className='text-green-900' /></h1>
-   </div>
-      </div>
-      </div>
-     
+    <div className='w-full mt-3 grid grid-cols-3 gap-6 '>
+   {courselists?.map((item) => (
+       <div className='w-[368px] h-[250px] lg:mt-3 lg:col-span-3 lg:w-full flex flex-col justify-center items-center  ' key={item.id}>
+       <Image loading='lazy' src={item.banner.url} width={360} height={360} alt='courses' className='rounded-lg object-cover'/>
+       <div className='w-full flex gap-2 justify-start lg:justify-center mt-2 items-center'>
+       <Avatar>
+       <AvatarImage src={item.authorimage.url} alt="@shadcn"  className='object-cover'/>
+       <AvatarFallback>CN</AvatarFallback>
+     </Avatar>
+    <div className='flex flex-col justify-center text-sm capitalize '>
+    <h1 className='font-semibold '>{item.name}</h1>
+    <h1 className='flex gap-1 justify-start items-center'>{item.author} | {item.totalChapters} Chapters <BadgeCheck size={15} className='text-green-900' /></h1>
+    </div>
+       </div>
+       </div>
+      
+   ))}
    
     </div>
   )
