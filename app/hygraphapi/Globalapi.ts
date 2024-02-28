6883f039f1b65dbb094f5b8661025a1cd81627d6
kwenpicasso <milezkwen@gmail.com>
2024-02-28 23:15:39 +0100

    hygraph singlecourse api data

--- app/hygraphapi/Globalapi.ts
+++ app/hygraphapi/Globalapi.ts
@@ -2,13 +2,14 @@ import { request, gql } from 'graphql-request'
 const MASTER_URL = `https://api-us-east-1-shared-usea1-02.hygraph.com/v2/${process.env.NEXT_PUBLIC_HYGRAPH_API_KEY}/master`
 
 
- const getAllCourses = async () => {
+export const getAllCourses = async () => {
     const query = gql`
     query MyQuery {
         course {
           id
           name
           author
+          free
           banner {
             url
           }
@@ -24,6 +25,34 @@ const result = await request(MASTER_URL, query);
 return result;
 }
 
-export default {
-    getAllCourses
+ export const getSingleCourse = async (id:any) => {
+    const query = gql`
+    query MyQuery {
+      courses(where: {id: "${id}"}) {
+        author
+        name
+        chapters {
+          ... on Chapter {
+            video {
+              url
+            }
+          }
+        }
+        description
+        totalChapters
+        authorimage {
+          url
+        }
+      }
+    }
+    
+      
+`
+const result = await request(MASTER_URL, query);
+return result;
 }
+
+// export default {
+//     getAllCourses,
+//     getSingleCourse
+// }
