cc0914530bf90070b0756c0ce3615c60c930473e
kwenpicasso <milezkwen@gmail.com>
2024-02-29 18:18:14 +0100

    api change

--- app/hygraphapi/Globalapi.ts
+++ app/hygraphapi/Globalapi.ts
@@ -1,7 +1,8 @@
 import { request, gql } from 'graphql-request'
+// hygraph master url
 const MASTER_URL = `https://api-us-east-1-shared-usea1-02.hygraph.com/v2/${process.env.NEXT_PUBLIC_HYGRAPH_API_KEY}/master`
 
-
+// hygraph api for all courses
 export const getAllCourses = async () => {
     const query = gql`
     query MyQuery {
@@ -25,11 +26,16 @@ const result = await request(MASTER_URL, query);
 return result;
 }
 
- export const getSingleCourse = async (id:any) => {
+
+
+// hygraph api for a single course with it ID
+ export const getSingleCourse = async (id:any,userEmail:any) => {
     const query = gql`
     query MyQuery {
       courses(where: {id: "${id}"}) {
         author
+        free
+        id
         name
         chapters {
           ... on Chapter {
@@ -44,6 +50,10 @@ return result;
           url
         }
       }
+      userEnrollCourses(where: {courseId: "${id}", userEmail: "${userEmail}"}) {
+        courseId
+        userEmail
+      }
     }
     
       
@@ -52,7 +62,34 @@ const result = await request(MASTER_URL, query);
 return result;
 }
 
-// export default {
-//     getAllCourses,
-//     getSingleCourse
-// }
+
+// hygraph api for mutation to add enrolled users with it ID and email address
+export const EnrollUser = async (id:any,userEmail:any) => {
+  const mutationQuery = gql`
+  mutation MyMutation {
+    createUserEnrollCourse(data: {courseId: "${id}", userEmail: "${userEmail}"}) {
+    
+      id
+    }
+  }
+  
+    
+`
+const result = await request(MASTER_URL, mutationQuery);
+return result;
+}
+
+// hygraph api for mutation to add enrolled users with it ID and email address
+export const PubishEnrollUser = async (id:any) => {
+  const mutationQuery = gql`
+  mutation MyMutation {
+    publishUserEnrollCourse(where: {id: "${id}"}) {
+      id
+    }
+  }
+  
+    
+`
+const result = await request(MASTER_URL, mutationQuery);
+return result;
+}
