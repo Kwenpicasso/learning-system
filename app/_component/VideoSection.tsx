3f8c93801ca74daac61f6eaddbb73e359c84e8ac
kwenpicasso <milezkwen@gmail.com>
2024-02-29 18:18:23 +0100

    comments

--- app/_component/VideoSection.tsx
+++ app/_component/VideoSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const VideoSection = ({video}:{video: string}) => {
-  // i am accepting the video as a props from the singlelist state 
+  // i am accepting the video as a props from the singlelist state passed from the parent component
   return (
     <div className='w-full h-[450px]  shadow-lg lg:h-[300px] flex justify-center '>
      <video width='800' height='300' src={video} controls controlsList='nodownload' typeof='video/mp4'></video>
