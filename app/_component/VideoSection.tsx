400fba4f6f9ce111adfcb530f07d88483d5b9e77
kwenpicasso <milezkwen@gmail.com>
2024-03-02 19:19:51 +0100

    video height

--- app/_component/VideoSection.tsx
+++ app/_component/VideoSection.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const VideoSection = ({video}:{video: string}) => {
   // i am accepting the video as a props from the singlelist state passed from the parent component
   return (
-    <div className='w-full h-[450px]  shadow-lg lg:h-[300px] flex justify-center '>
-     <video width='800' height='300' src={video} controls controlsList='nodownload' typeof='video/mp4'></video>
+    <div className='w-full h-[400px]  shadow-lg lg:h-[300px] flex justify-center '>
+     <video width='800' height='450' src={video} controls controlsList='nodownload' typeof='video/mp4'></video>
     </div>
   )
 }
